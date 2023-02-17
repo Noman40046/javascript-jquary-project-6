16cbfeb61fbb9ff8dc23246cd20359dc006d13af
Noman <118620280+Noman40046@users.noreply.github.com>
2023-02-18 00:06:24 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,83 @@
+
+
+/* counter1 start here----------------- */
+let countone= 1 ;
+
+$(document).ready(function()
+{
+    $('.button-33').click(function()
+    {
+        $('.text1').text(countone++);  
+    })
+    $('.button-333').click(function()
+    {
+        $('.text1').text(0);  
+    })
+}
+); 
+/* counter1 end here----------------- */
+
+
+/* counter2 start here----------------- */
+let counttwo= 1 ;
+
+$(document).ready(function()
+{
+    $('.button-34').click(function()
+    {
+        $('.text2').text(counttwo++); 
+    })
+    $('.button-344').click(function()
+    {
+        $('.text2').text(0);  
+    })
+}
+); 
+/* counter2 end here----------------- */
+
+
+/* counter3 start here----------------- */
+let countthree= 1 ;
+
+$(document).ready(function()
+{
+    $('.button-35').click(function()
+    {
+        $('.text3').text(countthree++); 
+    })
+    $('.button-355').click(function()
+    {
+        $('.text3').text(0);  
+    })
+}
+); 
+/* counter3 end here----------------- */
+
+
+
+
+
+
+
+
+// seclector-------------------- 
+/*
+ class,
+id,
+tag 
+*/
+//action-----------------------
+/* 
+hide,
+show
+ */
+
+//basic syntax of j-quary-------------------
+
+/*
+ $(document).ready(function()
+{
+    $('selector').action();
+} 
+); 
+*/
